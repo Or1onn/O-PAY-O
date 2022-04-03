2eb7b514aa087ec2c5406e05a3edac371ca7b6d7
Or1onn <salaxetdinov.orxan@gmail.com>
2022-04-03 23:22:23 +0400

    None

--- ViewModel/IncomeTypeViewModel.cs
+++ ViewModel/IncomeTypeViewModel.cs
@@ -10,11 +10,40 @@ namespace O_PAY_O.ViewModel
 {
     public class IncomeTypeViewModel : ViewModelBase
     {
-        private ObservableCollection<string>? incomes = new() { "VISA" };
+        private ObservableCollection<string>? incomes = new() { "VISA", "MASTER CARD" };
         public ObservableCollection<string>? Incomes
         {
             get { return incomes; }
             set => Set(ref incomes, value);
         }
+
+        private string? incomeText = "1";
+        public string? IncomeText
+        {
+            get
+            {
+                return incomeText;
+            }
+            set
+            {
+                incomeText = value;
+                Set(ref incomeText, value);
+            }
+        }
+
+        private string? expensesText;
+        public string? ExpensesText
+        {
+            get
+            {
+                return expensesText;
+            }
+            set
+            {
+                expensesText = value;
+                Set(ref expensesText, value);
+            }
+        }
+
     }
 }
--- obj/Debug/net6.0-windows/View/MainWindow.g.cs
+++ obj/Debug/net6.0-windows/View/MainWindow.g.cs
@@ -52,7 +52,7 @@ namespace O_PAY_O.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -68,7 +68,7 @@ namespace O_PAY_O.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
