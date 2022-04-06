295e62d01a7ba71a10d23b4ae935a9aba49df19b
Or1onn <salaxetdinovorxan@gmail.com>
2022-04-06 11:11:54 -0700

    V5.0 Release

--- obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
@@ -58,7 +58,7 @@ namespace O_PAY_O.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/O-PAY-O;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/O-PAY-O;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
